4a182eac7b279e56ac370ada40d4ed54e127cde4
Mohab <khalifamohab97@outlook.com>
2022-04-25 13:52:14 +0100

    added fields and constructor:

--- src/main/java/com/qa/movie_project/service/ReviewService.java
+++ src/main/java/com/qa/movie_project/service/ReviewService.java
@@ -1,5 +1,20 @@
 package com.qa.movie_project.service;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import com.qa.movie_project.repo.ReviewRepo;
+
+@Service
 public class ReviewService {
+
+	private ReviewRepo reviewRepo;
+	private ModelMapper modelMapper;
+
+	public ReviewService(ReviewRepo reviewRepo, ModelMapper modelMapper) {
+		super();
+		this.reviewRepo = reviewRepo;
+		this.modelMapper = modelMapper;
+	}
 
 }
