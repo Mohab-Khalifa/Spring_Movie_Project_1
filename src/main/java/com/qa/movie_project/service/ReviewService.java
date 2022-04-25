1a9396f72a0284ded70ce0551fc3efba94879993
Mohab <khalifamohab97@outlook.com>
2022-04-25 14:04:03 +0100

    added updateReview method

--- src/main/java/com/qa/movie_project/service/ReviewService.java
+++ src/main/java/com/qa/movie_project/service/ReviewService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import com.qa.movie_project.dto.NewReviewDTO;
 import com.qa.movie_project.dto.ReviewDTO;
+import com.qa.movie_project.dto.UpdateReviewDTO;
 import com.qa.movie_project.entity.Review;
 import com.qa.movie_project.repo.ReviewRepo;
 
@@ -63,6 +64,16 @@ public class ReviewService {
 		Review toSave = this.modelMapper.map(review, Review.class);
 		Review newReview = reviewRepo.save(toSave);
 		return this.toDTO(newReview);
+	}
+
+	public ReviewDTO updateReview(UpdateReviewDTO reviewDTO, int id) {
+		if (reviewRepo.existsById(id)) {
+			Review savedReview = reviewRepo.getById(id);
+			savedReview.setRating(reviewDTO.getRating());
+			savedReview.setComment(reviewDTO.getComment());
+			return this.toDTO(reviewRepo.save(savedReview));
+		}
+		throw new EntityNotFoundException("Review not found with id " + id);
 	}
 
 }
