c29314a8dad35b69285d2bdd447a634be2e0ed9e
Mohab <khalifamohab97@outlook.com>
2022-04-25 13:36:31 +0100

    added necessary fields

--- src/main/java/com/qa/movie_project/dto/UpdateReviewDTO.java
+++ src/main/java/com/qa/movie_project/dto/UpdateReviewDTO.java
@@ -1,5 +1,18 @@
 package com.qa.movie_project.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UpdateReviewDTO {
+
+	@NotNull
+	@Min(1)
+	@Max(10)
+	private int rating;
+
+	@Size(min = 0, max = 250, message = "The comments must contain a minimum of 10 characters and maximum of 250 characters")
+	private String comment;
 
 }
