3f0c4f931f0b5729a0422c6f782a6a089e36cd03
Mohab <khalifamohab97@outlook.com>
2022-04-25 17:55:13 +0100

    added GetMapping with for get reviews and get review methods

--- src/main/java/com/qa/movie_project/controller/ReviewController.java
+++ src/main/java/com/qa/movie_project/controller/ReviewController.java
@@ -1,5 +1,35 @@
 package com.qa.movie_project.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.movie_project.dto.ReviewDTO;
+import com.qa.movie_project.service.ReviewService;
+
+@RestController
+@RequestMapping(path = "/review")
 public class ReviewController {
 
+	private ReviewService reviewService;
+
+	@Autowired
+	public ReviewController(ReviewService reviewService) {
+		this.reviewService = reviewService;
+	}
+
+	@GetMapping
+	public ResponseEntity<List<ReviewDTO>> getReviews() {
+		return ResponseEntity.ok(reviewService.getReviews());
+	}
+
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<ReviewDTO> getReview(@PathVariable(name = "id") int id) {
+		return ResponseEntity.ok(reviewService.getReview(id));
+	}
 }
