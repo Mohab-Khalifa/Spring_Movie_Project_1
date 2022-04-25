2eb546934117231654cb1a95265fe0de54ba75bc
Mohab <khalifamohab97@outlook.com>
2022-04-25 11:51:18 +0100

    initial commit

---
+++ src/test/java/com/qa/intro_project/SpringMovieProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.intro_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMovieProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
