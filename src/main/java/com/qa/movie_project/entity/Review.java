63da474f8525ca256f327322afd1781e27ae6963
Mohab <khalifamohab97@outlook.com>
2022-04-25 12:31:00 +0100

    created review class and added fields

---
+++ src/main/java/com/qa/movie_project/entity/Review.java
@@ -0,0 +1,43 @@
+package com.qa.movie_project.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "review")
+public class Review {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@NotNull
+	@Min(1)
+	@Max(10)
+	private int rating;
+
+	@Size(min = 0, max = 250, message = "The comments must contain a minimum of 10 characters and maximum of 250 characters")
+	private String comment;
+
+	@NotNull
+	private LocalDate postedAt;
+
+	@ManyToOne(optional = true, fetch = FetchType.LAZY)
+	@JoinColumn(name = "movie_id", referencedColumnName = "id")
+	private Movie movie;
+
+}
