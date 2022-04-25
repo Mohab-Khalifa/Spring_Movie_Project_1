ea1aad8203dd396912fd8efa584b1978e946dc64
Mohab <khalifamohab97@outlook.com>
2022-04-25 16:24:55 +0100

    added movieRepo and reviewRepo as fields

--- src/main/java/com/qa/movie_project/configuration/DevStartupListener.java
+++ src/main/java/com/qa/movie_project/configuration/DevStartupListener.java
@@ -1,5 +1,16 @@
 package com.qa.movie_project.configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.qa.movie_project.repo.MovieRepo;
+import com.qa.movie_project.repo.ReviewRepo;
+
+@Profile("dev")
+@Configuration
 public class DevStartupListener {
+
+	private MovieRepo movieRepo;
+	private ReviewRepo reviewRepo;
 
 }
