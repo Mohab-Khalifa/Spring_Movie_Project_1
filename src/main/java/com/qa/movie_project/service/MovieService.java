7bbef994a128883153c929a2d09a21fd6a655898
Mohab <khalifamohab97@outlook.com>
2022-04-25 14:13:55 +0100

    added necessary fields

--- src/main/java/com/qa/movie_project/service/MovieService.java
+++ src/main/java/com/qa/movie_project/service/MovieService.java
@@ -1,5 +1,15 @@
 package com.qa.movie_project.service;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import com.qa.movie_project.repo.MovieRepo;
+
+@Service
 public class MovieService {
+
+	private MovieRepo movieRepo;
+	private ReviewService reviewService;
+	private ModelMapper modelMapper;
 
 }
