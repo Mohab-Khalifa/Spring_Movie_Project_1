00ec403f24c258f45fc3dc404a60f7bc46dcbe4e
Mohab <khalifamohab97@outlook.com>
2022-04-25 17:09:21 +0100

    added GetMapping for reviews

--- src/main/java/com/qa/movie_project/controller/MovieController.java
+++ src/main/java/com/qa/movie_project/controller/MovieController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.movie_project.dto.MovieDTO;
+import com.qa.movie_project.dto.ReviewDTO;
 import com.qa.movie_project.service.MovieService;
 
 @RestController
@@ -28,6 +30,19 @@ public class MovieController {
 	public ResponseEntity<List<MovieDTO>> getMovies() {
 		List<MovieDTO> responseBody = this.movieService.getMovies();
 		return new ResponseEntity<List<MovieDTO>>(responseBody, HttpStatus.OK);
+	}
+
+	// Read - gets a movie by it's id
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<MovieDTO> getMovie(@PathVariable(name = "id") int id) {
+		MovieDTO movie = movieService.getMovie(id);
+		return new ResponseEntity<>(movie, HttpStatus.OK);
+	}
+
+	// Read - gets the reviews of a specific movie by it's Id
+	@GetMapping(path = "/{id}/reviews")
+	public ResponseEntity<List<ReviewDTO>> getMovieReviews(@PathVariable(name = "id") int movieId) {
+		return ResponseEntity.ok(movieService.getMovieReviews(movieId));
 	}
 
 }
