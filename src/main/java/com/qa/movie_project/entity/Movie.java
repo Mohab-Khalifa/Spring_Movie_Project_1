51b3dbbca103459d42e85aa2e445f45f530b1f01
Mohab <khalifamohab97@outlook.com>
2022-04-25 12:18:54 +0100

    Created movie class and added fields

--- src/main/java/com/qa/movie_project/entity/Movie.java
+++ src/main/java/com/qa/movie_project/entity/Movie.java
@@ -1,5 +1,32 @@
 package com.qa.movie_project.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "movie")
 public class Movie {
+
+	@Id // tells Spring this is the Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT private integer id
+	private int id;
+
+	@NotNull
+	@NotBlank
+	private String title;
+
+	@NotNull
+	private String genre;
+
+	@NotNull
+	private Integer releaseYear;
+
+	@NotNull
+	private int runtime;
 
 }
