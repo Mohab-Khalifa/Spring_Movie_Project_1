61c916904b8c417985aca2bb99fe8db8a21806c9
Mohab <khalifamohab97@outlook.com>
2022-04-25 16:31:17 +0100

    added devStartupListener and onApplicationEvent methods to testit on postman

--- src/main/java/com/qa/movie_project/configuration/DevStartupListener.java
+++ src/main/java/com/qa/movie_project/configuration/DevStartupListener.java
@@ -1,8 +1,14 @@
 package com.qa.movie_project.configuration;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.qa.movie_project.entity.Movie;
+import com.qa.movie_project.entity.Review;
 import com.qa.movie_project.repo.MovieRepo;
 import com.qa.movie_project.repo.ReviewRepo;
 
@@ -12,5 +18,24 @@ public class DevStartupListener {
 
 	private MovieRepo movieRepo;
 	private ReviewRepo reviewRepo;
+
+	@Autowired
+	public DevStartupListener(MovieRepo movieRepo, ReviewRepo reviewRepo) {
+		this.movieRepo = movieRepo;
+		this.reviewRepo = reviewRepo;
+	}
+
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		List<Movie> movies = movieRepo.saveAll(
+				List.of(new Movie("Inception", "Thriller", 2010, 148), new Movie("White Chicks", "Comedy", 2004, 109)));
+
+		Movie movie = movies.stream().filter(m -> m.getTitle().equals("Inception")).findFirst().orElse(null);
+
+		List<Review> reviews = reviewRepo.saveAll(List.of(new Review(10, "10/10 is all you need to know!", movie),
+				new Review(5, "This movie is mediocre, would not recommend for cinema", movie),
+				new Review(8, "This movie is top tier", movie)));
+
+	}
 
 }
