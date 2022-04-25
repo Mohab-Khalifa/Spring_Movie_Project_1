d9401077e2c072e7c76ecf2cd6373515334273fc
Mohab <khalifamohab97@outlook.com>
2022-04-25 14:14:42 +0100

    aded constructor

--- src/main/java/com/qa/movie_project/service/MovieService.java
+++ src/main/java/com/qa/movie_project/service/MovieService.java
@@ -1,6 +1,7 @@
 package com.qa.movie_project.service;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qa.movie_project.repo.MovieRepo;
@@ -12,4 +13,11 @@ public class MovieService {
 	private ReviewService reviewService;
 	private ModelMapper modelMapper;
 
+	@Autowired
+	public MovieService(MovieRepo movieRepo, ReviewService reviewService, ModelMapper modelMapper) {
+		super();
+		this.movieRepo = movieRepo;
+		this.reviewService = reviewService;
+		this.modelMapper = modelMapper;
+	}
 }
