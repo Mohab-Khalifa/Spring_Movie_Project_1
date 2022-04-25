80bc23f0602b74c28449b122235df15ba7d4a860
Mohab <khalifamohab97@outlook.com>
2022-04-25 12:42:25 +0100

    added fields

--- src/main/java/com/qa/movie_project/dto/MovieDTO.java
+++ src/main/java/com/qa/movie_project/dto/MovieDTO.java
@@ -1,5 +1,23 @@
 package com.qa.movie_project.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class MovieDTO {
+
+	private int id;
+
+	@NotNull
+	@NotBlank
+	private String title;
+
+	@NotNull
+	private String genre;
+
+	@NotNull
+	private Integer releaseYear;
+
+	@NotNull
+	private int runtime;
 
 }
