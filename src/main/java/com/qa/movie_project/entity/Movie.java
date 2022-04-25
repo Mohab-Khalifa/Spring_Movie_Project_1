a2cdf2dcd969ce6112138121b27604023fed82b0
Mohab <khalifamohab97@outlook.com>
2022-04-25 12:20:39 +0100

    Added movie constructors

--- src/main/java/com/qa/movie_project/entity/Movie.java
+++ src/main/java/com/qa/movie_project/entity/Movie.java
@@ -1,5 +1,7 @@
 package com.qa.movie_project.entity;
 
+import java.util.ArrayList;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -28,5 +30,29 @@ public class Movie {
 
 	@NotNull
 	private int runtime;
+
+	public Movie() {
+		super();
+		this.reviews = new ArrayList<>();
+	}
+
+	public Movie(String title, String genre, Integer releaseYear, int runtime) {
+		super();
+		this.title = title;
+		this.genre = genre;
+		this.releaseYear = releaseYear;
+		this.runtime = runtime;
+		this.reviews = new ArrayList<>();
+	}
+
+	public Movie(int id, String title, String genre, Integer releaseYear, int runtime) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.genre = genre;
+		this.releaseYear = releaseYear;
+		this.runtime = runtime;
+		this.reviews = new ArrayList<>();
+	}
 
 }
