198875eb66d6d87f1e6931de122994e373f4b60a
Mohab <khalifamohab97@outlook.com>
2022-04-25 13:54:13 +0100

    added toDTO method and getReviews method

--- src/main/java/com/qa/movie_project/service/ReviewService.java
+++ src/main/java/com/qa/movie_project/service/ReviewService.java
@@ -1,8 +1,13 @@
 package com.qa.movie_project.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import com.qa.movie_project.dto.ReviewDTO;
+import com.qa.movie_project.entity.Review;
 import com.qa.movie_project.repo.ReviewRepo;
 
 @Service
@@ -17,4 +22,17 @@ public class ReviewService {
 		this.modelMapper = modelMapper;
 	}
 
+	private ReviewDTO toDTO(Review review) {
+		return modelMapper.map(review, ReviewDTO.class);
+	}
+
+	public List<ReviewDTO> getReviews() {
+		List<Review> reviews = reviewRepo.findAll();
+		List<ReviewDTO> dtos = new ArrayList<>();
+
+		for (Review review : reviews) {
+			dtos.add(this.toDTO(review));
+		}
+		return dtos;
+	}
 }
