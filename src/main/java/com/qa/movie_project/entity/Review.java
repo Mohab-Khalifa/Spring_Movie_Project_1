f9c88cc1d3b0cee9b0daf703062a9649f5b57c24
Mohab <khalifamohab97@outlook.com>
2022-04-25 12:33:08 +0100

    Added getters and setters

--- src/main/java/com/qa/movie_project/entity/Review.java
+++ src/main/java/com/qa/movie_project/entity/Review.java
@@ -76,4 +76,28 @@ public class Review {
 		this.postedAt = LocalDate.now();
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
 }
