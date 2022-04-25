0370f1eff11c47ae5b3175a063b42533a191d2aa
Mohab <khalifamohab97@outlook.com>
2022-04-25 13:11:58 +0100

    added fields

--- src/main/java/com/qa/movie_project/dto/ReviewDTO.java
+++ src/main/java/com/qa/movie_project/dto/ReviewDTO.java
@@ -1,5 +1,17 @@
 package com.qa.movie_project.dto;
 
+import java.time.LocalDate;
+
 public class ReviewDTO {
+
+	private int id;
+
+	private int rating;
+
+	private String comment;
+
+	private MovieDTO movieDTO;
+
+	private LocalDate postedAt;
 
 }
