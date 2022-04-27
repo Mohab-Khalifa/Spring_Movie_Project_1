93f4811e3030039d2e92923af01acd2ff235c78f
Mohab <khalifamohab97@outlook.com>
2022-04-27 10:59:11 +0100

    added @BeforeEach annotation for MovieServiceUnitTest class

--- src/test/java/com/qa/movie_project/service/MovieServiceUnitTest.java
+++ src/test/java/com/qa/movie_project/service/MovieServiceUnitTest.java
@@ -1,5 +1,39 @@
 package com.qa.movie_project.service;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import com.qa.movie_project.dto.MovieDTO;
+import com.qa.movie_project.entity.Movie;
+import com.qa.movie_project.repo.MovieRepo;
+
+@ExtendWith(MockitoExtension.class)
 public class MovieServiceUnitTest {
+
+	@Mock
+	private MovieRepo movieRepo;
+
+	@Mock
+	private ModelMapper modelMapper;
+
+	@InjectMocks
+	private MovieService movieService;
+
+	private List<Movie> movies;
+	private List<MovieDTO> movieDTOs;
+
+	@BeforeEach
+	public void init() {
+		movies = List.of(new Movie(1, "Interstellar", "Thriller", 2014, 169),
+				new Movie(1, "Toy Story", "Animation", 1995, 81));
+		movieDTOs = List.of(new MovieDTO(1, "Interstellar", "Thriller", 2014, 169),
+				new MovieDTO(2, "Toy Story", "Animation", 1995, 81));
+	}
 
 }
